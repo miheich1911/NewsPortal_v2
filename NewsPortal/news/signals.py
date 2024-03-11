d7ff58c659e1695c82cd5eb2a2e72cd2d3a91274
Дмитрий Михеев <143932457+miheich1911@users.noreply.github.com>
2024-03-11 12:49:53 +0300

    Update signals.py

--- NewsPortal/news/signals.py
+++ NewsPortal/news/signals.py
@@ -28,13 +28,13 @@ def send_notifications(preview, pk, title, subscribers):
 
 
 @receiver(m2m_changed, sender=PostCategory)
-def notify_about_new_post(sender, instance, subscribers, **kwargs):
+def notify_about_new_post(sender, instance, **kwargs):
     if kwargs['action'] == 'post_add':
         categories = instance.category.all()
         subscribers_emails = []
 
         for cat in categories:
-            subscribers += cat.subscribers.all()
+            subscribers = cat.subscribers.all()
             subscribers_emails += [s.email for s in subscribers]
 
         send_notifications(instance.preview(), instance.pk, instance.title, subscribers_emails)
